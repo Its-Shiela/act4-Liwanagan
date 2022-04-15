256761efebe679796676f0e0bb1dc28e6dde69b8
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-04-15 00:02:08 -0900

    first song added

---
+++ song1.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" style="color: #000" href="#page-top"> Don Quixote </a>
+                <button method="GET" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <section class="bg-image"
+                 style = "background-image: url(images/photo_7.png);height: 1920vh width: 1080vw" id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-dark">PAPER RINGS</h2>
+                        <p class="lead">Taylor Swift</p>
+                        <p>The moon is high</p>
+                        <p>Like your friends were the night that we first met</p>
+                        <p>Went home and tried to stalk you on the internet</p>
+                        <p>Now I've read all of the books beside your bed</p>
+                        <p>The wine is cold</p>
+                        <p>Like the shoulder that I gave you in the street</p>
+                        <p>Cat and mouse for a month or two or three</p>
+                        <p>Now I wake up in the night and watch you breathe</p>
+                        <p>(Ayy)</p>
+                        <p>Kiss me once 'cause you know I had a long night (Oh)</p>
+                        <p>Kiss me twice 'cause it's gonna be alright (Uh)</p>
+                        <p>Three times 'cause I waited my whole life (1, 2, 1, 2, 3, 4)</p>
+                        <p>I like shiny things, but I'd marry you with paper rings</p>
+                        <p>Uh huh, that's right</p>
+                        <p>Darling, you're the one I want, and</p>
+                        <p>I hate accidents except when we went from friends to this</p>
+                        <p>Uh huh, that's right</p>
+                        <p>Darling, you're the one I want</p>
+                        <p>In paper rings in picture frames in dirty dreams</p>
+                        <p>Oh, you're the one I want</p>
+                        <p>In the winter, in the icy outdoor pool</p>
+                        <p>When you jumped in first, I went in too</p>
+                        <p>I'm with you even if it makes me blue</p>
+                        <p>Which takes me back</p>
+                        <p>To the color that we painted your brother's wall</p>
+                        <p>Honey, without all the exes, fights, and flaws</p>
+                        <p>We wouldn't be standing here so tall, so</p>
+                        <p>(Ayy)</p>
+                        <p>Kiss you once 'cause I know you had a long night (Oh)</p>
+                        <p>Kiss you twice 'cause it's gonna be alright (Uh)</p>
+                        <p>Three times 'cause you waited your whole life (1, 2, 1, 2, 3, 4)</p>
+                        <p>Ah</p>
+                        <p>I like shiny things, but I'd marry you with paper rings</p>
+                        <p>Uh huh, that's right</p>
+                        <p>Darling, you're the one I want, and</p>
+                        <p>I hate accidents except when we went from friends to this</p>
+                        <p>Uh huh, that's right</p>
+                        <p>Darling, you're the one I want</p>
+                        <p>In paper rings in picture frames in dirty dreams</p>
+                        <p>Oh, you're the one I want</p>
+                        <p>I want to drive away with you</p>
+                        <p>I want your complications too</p>
+                        <p>I want your dreary Mondays</p>
+                        <p>Wrap your arms around me, baby boy</p>
+                        <p>I want to drive away with you</p>
+                        <p>I want your complications too</p>
+                        <p>I want your dreary Mondays</p>
+                        <p>Wrap your arms around me, baby boy</p>
+                        <p>Uh huh</p>
+                        <p>I like shiny things, but I'd marry you with paper rings</p>
+                        <p>Uh huh, that's right, you're the one I want</p>
+                        <p>I hate accidents except when we went from friends to this</p>
+                        <p>Ah-ah, darling, you're the one I want</p>
+                        <p>I like shiny things, but I'd marry you with paper rings</p>
+                        <p>Uh huh, that's right</p>
+                        <p>Darling, you're the one I want, and</p>
+                        <p>I hate accidents except when we went from friends to this</p>
+                        <p>Uh huh, that's right</p>
+                        <p>Darling, you're the one I want</p>
+                        <p>In paper rings in picture frames in all my dreams</p>
+                        <p>You're the one I want</p>
+                        <p>In paper rings in picture frames in all my dreams</p>
+                        <p>Oh, you're the one I want</p>
+                        <p>You're the one I want, one I want</p>
+                        <p>You're the one I want, one I want</p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: coral;
+   color: white;
+   text-align: center;
+}
+</style>
+
+        <footer 
+            <div class = "footer"class="container px-4"><p>Copyright &copy; 2022. Shiela Mae Liwanagan</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+        
+</html>
