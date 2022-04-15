d0101cc6d86185530582c49ced646e2d39685ad6
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-04-15 09:42:36 -0900

    fourth song added

---
+++ song4.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" style="color: #000" href="#page-top"> Don Quixote </a>
+                <button method="GET" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <section class="bg-image"
+                 style = "background-image: url(images/202.png);height: 100vh width: 100vw" id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-dark">SINCERITY IS SCARY</h2>
+                        <p class="lead">The 1975</p>
+                        <p>And irony is ok, I suppose, culture is to blame</p>
+                        <p>You try and mask your pain in the most postmodern way</p>
+                        <p>You lack substance when you say something like "oh what a shame"</p>
+                        <p>It's just a self-referential way that stops you having to be human</p>
+                        <p>I'm assuming you'll balloon when you remove the dirty spoon</p>
+                        <p>And start consuming like a human, that's what I am assuming</p>
+                        <p>I'm sure that you're not just another girl</p>
+                        <p>I'm sure that you're gonna say that that was sexist</p>
+                        <p>I feel like you're running out of all the things I liked you for</p>
+                        <p>Why can't we be friends, when we are lovers?</p>
+                        <p>'Cause it always ends with us hating each other</p>
+                        <p>Instead of calling me out you should be pulling me in</p>
+                        <p>"I've just got one more thing to say"</p>
+                        <p>And why would you believe you could control how you're perceived</p>
+                        <p>When at your best you're intermediately versed in your own feelings?</p>
+                        <p>Keep on putting off conceiving, it's only you that you're deceiving</p>
+                        <p>Oh, don't have a child, don't cramp your style, I'll leave it</p>
+                        <p>Why can't we be friends, when we are lovers?</p>
+                        <p>'Cause it always ends with us hating each other</p>
+                        <p>Instead of calling me out you should be pulling me in</p>
+                        <p>"I've just got one more thing to say"</p>
+                        <p>I'm just pissed off because you pied me off</p>
+                        <p>After your show when you let go</p>
+                        <p>Of my hand in front of some sket</p>
+                        <p>Who wanted bit on your</p>
+                        <p>Nah, I'm just messing</p>
+                        
+
+                        
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: coral;
+   color: white;
+   text-align: center;
+}
+</style>
+
+        <footer 
+            <div class = "footer"class="container px-4"><p>Copyright &copy; 2022. Shiela Mae Liwanagan</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+        
+</html>
