9213992edc3fce1b1db362dd9791427e026ecb05
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-04-15 00:10:55 -0900

    second song added

---
+++ song2.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" style="color: #000" href="#page-top"> Don Quixote </a>
+                <button method="GET" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <section class="bg-image"
+                 style = "background-image: url(images/photo_10.png);height: 1920vh width: 1080vw" id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-dark">Spring Day (봄날)</h2>
+                        <p class="lead">BTS</p>
+                        <p>Bogo shipda</p>
+                        <p>Ireoke malhanikka deo</p>
+                        <p>Bogo shipda</p>
+                        <p>Neohi sajineul bogo isseodo</p>
+                        <p>Bogo shipda</p>
+                        <p>Neomu yasokhan shigan</p>
+                        <p>Naneun uriga mibda</p>
+                        <p>Ijen eolgul han beon boneun geotto</p>
+                        <p>Himdeureojin uriga</p>
+                        <p>Yeogin ontong gyeoul ppuniya</p>
+                        <p>Palworedo gyeouri wa</p>
+                        <p>Maeumeun shiganeul dallyeogane</p>
+                        <p>Hollo nameun seolgukyeolcha</p>
+                        <p>Ni son jabgo</p>
+                        <p>Jigu bandaepyeonkkaji ga</p>
+                        <p>Gyeoureul kkeutnaegopa</p>
+                        <p>Geuriumdeuri</p>
+                        <p>Eolmana nuncheoreom naeryeoya</p>
+                        <p>Geu bomnari olkka</p>
+                        <p>Friend</p>
+                        <p>Heogongeul tteodoneun</p>
+                        <p style="color:white;">Jageun meonjicheoreom</p>
+                        <p style="color:white;">Jageun meonjicheoreom</p>
+                        <p style="color:white;">Nallineun nuni naramyeon</p>
+                        <p style="color:white;">Jogeum deo ppalli nege</p>
+                        <p>Daheul su isseul tende</p>
+                        <p>Nunkkochi tteoreojyeoyo</p>
+                        <p>Tto jogeumsshik meoreojyeoyo</p>
+                        <p>Bogo shipda (bogo shipda)</p>
+                        <p>Bogo shipda (bogo shipda)</p>
+                        <p>Eolmana gidaryeoya</p>
+                        <p>Tto myeot bameul deo saewoya</p>
+                        <p>Neol boge doelkka (neol boge doelkka)</p>
+                        <p>Mannage doelkka (mannage doelkka)</p>
+                        <p>Chuun gyeoul kkeuteul jina</p>
+                        <p>Dashi bomnari ol ttaekkaji</p>
+                        <p>Kkot piul ttaekkaji</p>
+                        <p>Geugose jom deo meomulleojwo</p>
+                        <p>Meomulleojwo</p>
+                        <p>Niga byeonhan geonji</p>
+                        <p>(Niga byeonhan geonji)</p>
+                        <p>Animyeon naega byeonhan geonji</p>
+                        <p>(Naega byeonhan geonji)</p>
+                        <p>I sungan heureuneun shiganjocha miwo</p>
+                        <p>Uriga byeonhan geoji mwo</p>
+                        <p>Moduga geureon geoji mwo</p>
+                        <p>Geurae mibda niga</p>
+                        <p>Neon tteonatjiman</p>
+                        <p>Dan harudo neoreul</p>
+                        <p>Ijeun jeogi eopseotji nan</p>
+                        <p>Soljikhi bogo shipeunde</p>
+                        <p>Iman neoreul jiulge</p>
+                        <p>Geuge neol wonmanghagibodan</p>
+                        <p style="color:white;">Deol apeunikka</p>
+                        <p style="color:white;">Shirin neol bureonae bonda</p>
+                        <p style="color:white;">Yeongicheoreom hayan yeongicheoreom</p>
+                        <p style="color:white;">Malloneun jiunda haedo</p>
+                        <p>Sashil nan ajik neol bonaeji mothaneunde</p>
+                        <p>Nunkkochi tteoreojyeoyo</p>
+                        <p>Tto jogeumsshik meoreojyeoyo</p>
+                        <p>Bogo shipda (bogo shipda)</p>
+                        <p>Bogo shipda (bogo shipda)</p>
+                        <p>Eolmana gidaryeoya</p>
+                        <p>Tto myeot bameul deo saewoya</p>
+                        <p>Neol boge doelkka (neol boge doelkka)</p>
+                        <p>Mannage doelkka (mannage doelkka)</p>
+                        <p>You know it all</p>
+                        <p>You're my best friend</p>
+                        <p>Achimeun dashi ol geoya</p>
+                        <p>Eotteon eodumdo eotteon gyejeoldo</p>
+                        <p>Yeongweonhal sun eopseunikka</p>
+                        <p>Beotkkochi pinabwayo</p>
+                        <p>I gyeouldo kkeuchi nayo</p>
+                        <p>Bogo shipda (bogo shipda)</p>
+                        <p>Bogo shipda (bogo shipda)</p>
+                        <p>Jogeumman gidarimyeon</p>
+                        <p>Myeochil bamman deo saeumyeon</p>
+                        <p>Mannareo galge (mannareo galge)</p>
+                        <p>Derireo galge (derireo galge)</p>
+                        <p>Chuun gyeoul kkeuteul jina</p>
+                        <p>Dashi bomnari ol ttaekkaji</p>
+                        <p>Kkot piul ttaekkaji</p>
+                        <p>Geugose jom deo meomulleojwo</p>
+                        <p>Meomulleojwo</p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: coral;
+   color: white;
+   text-align: center;
+}
+</style>
+
+        <footer 
+            <div class = "footer"class="container px-4"><p>Copyright &copy; 2022. Shiela Mae Liwanagan</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+        
+</html>
